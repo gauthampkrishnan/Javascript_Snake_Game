01427c52f5a0ccaf9b1be3adc985104a28c6aeb4
gauthampkrishnan <gauthamkrishnanp@gmail.com>
2020-05-08 19:53:38 +0530

    Javascript based Snake game for web browser

---
+++ snake.js
@@ -0,0 +1,42 @@
+function init(){
+    canvas = document.getElementById("mycanvas");
+    W=H=canvas.width = canvas.height=1000;
+    pen = canvas.getContext('2d');
+    cs=66;
+    snake = {
+        init_len:5,
+        color:"blue",
+        cells:[],
+        direction:"right",
+        createSnake: function(){
+            for(var i =this.init_len;i>=0;i--){
+                this.cells.push({x:i,y:0});
+            }
+        },
+        drawSnake: function(){
+            for(var i=0;i<this.cells.length;i++){
+            pen.fillRect(this.cells[i].x*cs-2,this.cells[i].y*cs,cs-1,cs-1)
+            }
+        }
+    }
+    snake.createSnake()
+}
+
+
+
+function draw(){
+    snake.drawSnake()
+}
+
+
+function update(){
+
+}
+
+function gameloop(){
+    draw()
+    update()
+}
+
+init();
+var f = setInterval(gameloop,100)
